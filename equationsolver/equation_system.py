70d74b2c4f2dfdb214f4f8fcbbc3931c4d98f9b2
Miguel Angel Julian <miguel.a.j82@gmail.com>
2018-05-16 19:53:58 +0200

    Implementacion equation_system

--- equationsolver/equation_system.py
+++ equationsolver/equation_system.py
@@ -9,40 +9,51 @@ class EquationSystem:
         self._solution_method = None
 
     def add(self, equation):
-        pass
+        self._equation_list.append(equation.clon())
 
     def set(self, solution_method):
-        pass
+        self._solution_method = solution_method
+        self._solution_method.set(self)
 
     def resolve(self):
-        pass
+        name_set = self.get_name_set()
+        while set(self._solutions.keys()) != name_set:
+            self._solution_method.resolve()
 
     def get_name_set(self):
-        return None
+        return self._get_equation_list_name_set(self._equation_list)
 
     def __get(self, index):
-        return None
+        return self._equation_list[index]
 
     def get_last_before(self, before):
-        return None
+        return self._equation_list[before - 1]
 
     def get_last(self):
-        return None
+        return self._equation_list[-1]
 
     def copy_before_before(self, before):
-        pass
+        self._equation_list = self._equation_list[:before] + self.__get(before).clon() + self._equation_list[before:]
 
     def copy_before(self):
-        pass
+        self.add(self.get_last())
 
     def set_solution(self, first_name, equation):
-        pass
+        self._solutions[first_name] = equation
+
+    def _get_equation_list_name_set(self, equation_list):
+        name_set = set()
+        for equation in equation_list:
+            name_set.update(equation.get_name_set())
+        return name_set
 
     def get_solution(self, name):
-        raise NotSolved
+        if name not in self._solutions:
+            raise NotSolved
+        return self._solutions[name]
 
     def equal(self):
         return False
 
     def __str__(self):
-        return ''
+        return '\n'.join(str(equation) for equation in self._equation_list)
