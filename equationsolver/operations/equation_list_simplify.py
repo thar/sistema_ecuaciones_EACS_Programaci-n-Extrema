b35684cb514b23b7684732cef9cbe606e1867062
Miguel Angel Julian <miguel.a.j82@gmail.com>
2018-05-19 00:25:54 +0200

    Simplificar lista de equaciones borra las equaciones cte=cte

--- equationsolver/operations/equation_list_simplify.py
+++ equationsolver/operations/equation_list_simplify.py
@@ -1,3 +1,4 @@
+from equationsolver.equation import Side
 from equationsolver.operations.equation_list_operation_applier import EquationListOperationApplier
 from equationsolver.operations.equation_simplify import EquationSimplify
 
@@ -5,3 +6,15 @@ from equationsolver.operations.equation_simplify import EquationSimplify
 class EquationListSimplify(EquationListOperationApplier):
     def __init__(self):
         EquationListOperationApplier.__init__(self, EquationSimplify())
+
+    def apply(self):
+        EquationListOperationApplier.apply(self)
+        self._remove_unneded_expressions()
+
+    def _remove_unneded_expressions(self):
+        unnedded_expressions = []
+        for eq in self._equation_list:
+            if len(eq.get_name_set()) == 0 and eq.get_value_constant(Side.left) == eq.get_value_constant(Side.right):
+                unnedded_expressions.append(eq)
+        for eq in unnedded_expressions:
+            self._equation_list.remove(eq)
