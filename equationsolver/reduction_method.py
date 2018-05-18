7fa3187ef7eddbdf1366cb79895a8f548b836348
Miguel Angel Julian <miguel.a.j82@gmail.com>
2018-05-18 19:58:48 +0200

    El multiplo comun a partir del clon de equation_system

--- equationsolver/reduction_method.py
+++ equationsolver/reduction_method.py
@@ -8,6 +8,7 @@ class ReductionMethod(SolutionMethod):
         self._variable_to_reduce = None
         self._common_multiple = 1.0
         self._equation_to_resolve = None
+        self._equation_system_to_recurse = None
 
     def resolve(self):
         self._equation_system.simplify()
@@ -29,6 +30,7 @@ class ReductionMethod(SolutionMethod):
         self._equation_system.set_solution(self._variable_to_reduce, self._equation_to_resolve)
 
     def _resolve_recursive(self, name_set):
+        self._equation_system_to_recurse = self._equation_system.clon()
         self._get_reducible_variable_common_multiple()
         self._multiply_equations_to_reach_common_multiple()
         self._store_equation_to_reduce()
@@ -47,8 +49,8 @@ class ReductionMethod(SolutionMethod):
         self._equation_to_resolve.simplify()
 
     def _get_reducible_variable_common_multiple(self):
-        self._equation_system.normalize()
-        reducible_variable_values = self._equation_system.get_variable_name_values(Side.left, self._variable_to_reduce)
+        self._equation_system_to_recurse.normalize()
+        reducible_variable_values = self._equation_system_to_recurse.get_variable_name_values(Side.left, self._variable_to_reduce)
         self._common_multiple = 1.0
         for value in reducible_variable_values:
             if value != 0:
