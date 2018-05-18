6a339d3639f9ca91059f6ff8493bdf91dd5789aa
Miguel Angel Julian <miguel.a.j82@gmail.com>
2018-05-18 17:17:55 +0200

    Cambiar test de reduction_method para usar EquationSystemSolver

--- tests/test_reduction_method.py
+++ tests/test_reduction_method.py
@@ -3,6 +3,7 @@ import unittest
 from equationsolver.constant_builder import ConstantBuilder
 from equationsolver.equation_builder import EquationBuilder
 from equationsolver.equation_system_builder import EquationSystemBuilder
+from equationsolver.equation_system_solver import EquationSystemSolver
 from equationsolver.reduction_method import ReductionMethod
 from equationsolver.variable_builder import VariableBuilder
 
@@ -16,8 +17,11 @@ class ReductionMethodTestCase(unittest.TestCase):
         a.add_equation(EquationBuilder.one_equals_y())
         eqs.add(a)
         eqs.add(b)
-        eqs.set(ReductionMethod())
-        eqs.resolve()
-        self.assertTrue(eqs.get_solution('x').equal(EquationBuilder.x_equals_1()))
-        self.assertTrue(eqs.get_solution('y').equal(EquationBuilder.y_equals_1()))
-        self.assertTrue(eqs.get_solution('z').equal(EquationBuilder().left_term(VariableBuilder().name('z').build()).right_term(ConstantBuilder().value(2).build()).build()))
+        reduction_method = ReductionMethod()
+        eqs.set(reduction_method)
+        equation_system_solver = EquationSystemSolver(eqs, reduction_method)
+        eqs.set_equation_system_solver(equation_system_solver)
+        equation_system_solver.resolve()
+        self.assertTrue(equation_system_solver.get_solution('x').equal(EquationBuilder.x_equals_1()))
+        self.assertTrue(equation_system_solver.get_solution('y').equal(EquationBuilder.y_equals_1()))
+        self.assertTrue(equation_system_solver.get_solution('z').equal(EquationBuilder().left_term(VariableBuilder().name('z').build()).right_term(ConstantBuilder().value(2).build()).build()))
