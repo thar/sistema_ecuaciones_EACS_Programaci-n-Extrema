498e05c1155e859803dac2e309eb070b9aaa32a2
Miguel Angel Julian <miguel.a.j82@gmail.com>
2018-05-18 17:25:01 +0200

    setear sistema de ecuaciones en SolutionMethod desde EquationSystemSolver

--- equationsolver/equation_system_solver.py
+++ equationsolver/equation_system_solver.py
@@ -2,6 +2,7 @@ class EquationSystemSolver:
     def __init__(self, equation_system, solution_method):
         self._equation_system = equation_system
         self._solution_method = solution_method
+        self._solution_method.set(self._equation_system)
         self._solutions = {}
 
     def resolve(self):
