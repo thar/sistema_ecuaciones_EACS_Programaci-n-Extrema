a66fc59ae000bdc5f36b8f5a97702e541a74c9a6
Miguel Angel Julian <miguel.a.j82@gmail.com>
2018-05-21 13:46:51 +0200

    Crear una abstracción para las soluciones programadas con un algoritmo recursivo

---
+++ equationsolver/recursive_solution_method.py
@@ -0,0 +1,53 @@
+from equationsolver.equation import Equation, Side
+from equationsolver.operations.equation_list_simplify import EquationListSimplify
+from equationsolver.solution_method import SolutionMethod
+
+
+class RecursiveSolutionMethod(SolutionMethod):
+    def __init__(self):
+        SolutionMethod.__init__(self)
+        self._variable_to_reduce = None
+        self._equation_to_resolve = None
+        self._solution_method_recurse = None
+
+    def resolve(self):
+        self._equation_system.apply_operation(EquationListSimplify())
+        self._solutions.update(self._equation_system.pop_solution_equations())
+        name_set = self._equation_system.get_name_set()
+        if len(name_set) == 0:
+            return
+        self._variable_to_reduce = name_set.pop()
+        self._preprocess_input_equation_system()
+        self._store_equation_to_resolve()
+        if len(name_set) > 0:
+            self._resolve_recursive()
+            self._merge_solutions()
+        else:
+            self._resolve_last_iteration()
+        self._compute_solution()
+
+    def _preprocess_input_equation_system(self):
+        raise NotImplemented
+
+    def _resolve_recursive(self):
+        raise NotImplemented
+
+    def _resolve_last_iteration(self):
+        raise NotImplemented
+
+    def _store_equation_to_resolve(self):
+        self._equation_to_resolve = self._equation_system.get_equation_that_contains_name_set(
+            {self._variable_to_reduce}).clon()
+
+    def _compute_solution(self):
+        self._equation_to_resolve.apply_operation(Equation.VariableIsolator(self._variable_to_reduce))
+        self.set_solution(self._variable_to_reduce, self._equation_to_resolve)
+
+    def _merge_solutions(self):
+        for variable_name in self._solution_method_recurse.get_solutions_name_set():
+            variable_solution_equation = self._solution_method_recurse.get_solution(variable_name)
+            variable_solution_value = variable_solution_equation.get_value_constant(Side.right)
+            self.set_solution(variable_name, variable_solution_equation)
+            if variable_name in self._equation_to_resolve.get_name_set():
+                self._equation_to_resolve.apply_operation(Equation.ValueApplier(variable_name, variable_solution_value))
+        self._equation_to_resolve.apply_operation(Equation.EquationSimplifyer())
--- equationsolver/reduction_method.py
+++ equationsolver/reduction_method.py
@@ -3,10 +3,11 @@ from equationsolver.operations.common_multiple_variable_setter import CommonMult
 from equationsolver.operations.equation_list_equation_sum import EquationListEquationSum
 from equationsolver.operations.equation_list_operation_applier import EquationListOperationApplier
 from equationsolver.operations.equation_list_simplify import EquationListSimplify
+from equationsolver.recursive_solution_method import RecursiveSolutionMethod
 from equationsolver.solution_method import SolutionMethod
 
 
-class ReductionMethod(SolutionMethod):
+class ReductionMethod(RecursiveSolutionMethod):
     def __init__(self):
         SolutionMethod.__init__(self)
         self._variable_to_reduce = None
@@ -15,28 +16,14 @@ class ReductionMethod(SolutionMethod):
         self._equation_to_reduce = None
         self._solution_method_recurse = None
 
-    def resolve(self):
-        self._equation_system.apply_operation(EquationListSimplify())
-        self._solutions.update(self._equation_system.pop_solution_equations())
-        name_set = self._equation_system.get_name_set()
-        if len(name_set) == 0:
-            return
-        self._variable_to_reduce = name_set.pop()
+    def _preprocess_input_equation_system(self):
         self._equation_system.apply_operation(
             EquationListOperationApplier(Equation.VariableMover(self._variable_to_reduce, Side.left)))
         self._equation_system.apply_operation(EquationListSimplify())
-        self._store_equation_to_resolve()
-        if len(name_set) > 0:
-            self._resolve_recursive()
-            self.merge_solutions()
-        else:
-            self._equation_to_resolve = self._equation_system.get_equation_that_contains_name_set(
-                {self._variable_to_reduce}).clon()
-        self._compute_solution()
 
-    def _compute_solution(self):
-        self._equation_to_resolve.apply_operation(Equation.VariableIsolator(self._variable_to_reduce))
-        self.set_solution(self._variable_to_reduce, self._equation_to_resolve)
+    def _resolve_last_iteration(self):
+        self._equation_to_resolve = self._equation_system.get_equation_that_contains_name_set(
+            {self._variable_to_reduce}).clon()
 
     def _resolve_recursive(self):
         equation_system_to_recurse = self._equation_system.clon()
@@ -52,19 +39,6 @@ class ReductionMethod(SolutionMethod):
         self._equation_to_reduce = equation_system_to_recurse.get_equation_that_contains_name_set(
             {self._variable_to_reduce}).clon()
 
-    def _store_equation_to_resolve(self):
-        self._equation_to_resolve = self._equation_system.get_equation_that_contains_name_set(
-            {self._variable_to_reduce}).clon()
-
     def _reduce_all_equations(self, equation_system_to_recurse):
         self._equation_to_reduce.multiply(-1.0)
         equation_system_to_recurse.apply_operation(EquationListEquationSum(self._equation_to_reduce))
-
-    def merge_solutions(self):
-        for variable_name in self._solution_method_recurse.get_solutions_name_set():
-            variable_solution_equation = self._solution_method_recurse.get_solution(variable_name)
-            variable_solution_value = variable_solution_equation.get_value_constant(Side.right)
-            self.set_solution(variable_name, variable_solution_equation)
-            if variable_name in self._equation_to_resolve.get_name_set():
-                self._equation_to_resolve.apply_operation(Equation.ValueApplier(variable_name, variable_solution_value))
-        self._equation_to_resolve.apply_operation(Equation.EquationSimplifyer())
